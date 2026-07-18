dce1247480e7975c564421f870bd26b7403792f8
Thai Vo <87766110+VQTss@users.noreply.github.com>
2026-07-18 15:19:31 +0900

    Solve la-vector-projection

---
+++ study-plans/math-linear-algebra/la-vector-projection/la-vector-projection.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def vector_projection(u, v):
+    """
+    Returns: float64 array, the projection of u onto v.
+    """
+    u = np.asarray(u,dtype=float)
+    v = np.asarray(v,dtype=float)
+    scalar = np.dot(u,v) / np.dot(v,v)
+    return scalar * v
